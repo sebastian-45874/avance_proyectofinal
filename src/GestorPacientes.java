5936749be50d3c9a7a6fd07349a22c83e823fce4
andresASCA <s4npolas@gmail.com>
2025-11-30 21:21:45 -0500

    Documentacion Clase Gestor Paciente

--- src/GestorPacientes.java
+++ src/GestorPacientes.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
+//Permite agregar pacientes, buscarlos por número de cédula y obtener la lista completa
 public class GestorPacientes {
-
+//Lista que almacena todos los pacientes registrados
     private List<Paciente> listaPacientes;
-
+//Constructor que inicializa la lista de pacientes
     public GestorPacientes() {
         listaPacientes = new ArrayList<Paciente>();
     }
-
+//Agrega un paciente a la lista siempre que su cédula no esté registrada previamente
     public int agregarPaciente(Paciente p) {
         for (Paciente x : listaPacientes) {
             if (x.getCedula().equals(p.getCedula())) {
@@ -18,7 +19,7 @@ public class GestorPacientes {
         listaPacientes.add(p);
         return 1;
     }
-
+//Busca un paciente por su número de cédula
     public Paciente buscarPorCedula(String cedula) {
         for (Paciente p : listaPacientes) {
             if (p.getCedula().equals(cedula)) {
@@ -27,7 +28,7 @@ public class GestorPacientes {
         }
         return null;
     }
-
+//Obtiene la lista completa de pacientes registrados
     public List<Paciente> getListaPacientes() {
         return listaPacientes;
     }
